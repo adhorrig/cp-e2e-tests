5ac7aaca7439018d079d109786e07334fc6db000
Adam <adam@coderdojo.org>
2016-02-23 10:23:11 +0000

    session description and ticket type added

--- test/event.js
+++ test/event.js
@@ -23,6 +23,8 @@ module.exports = {
 		eventPage.expect.element('@city').to.be.present;
 		eventPage.expect.element('@address').to.be.present;
 		eventPage.expect.element('@sessionValidate').to.be.present;
+		eventPage.expect.element('@session-description').to.be.present;
+		eventPage.expect.element('@ticketType').to.be.present;
 		eventPage.expect.element('@session-{{ sessionIndex }}-sessionName').to.be.present;
 		eventPage.expect.element('@session-{{ sessionIndex }}-ticketName-{{ ticketIndex }}').to.be.present;
 		eventPage.expect.element('session-{{ sessionIndex }}-ticketQuantity-{{ ticketIndex }}').to.be.present;
