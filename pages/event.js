39e4248704c7479792eba141d61e30a93cabdd85
Adam <adam@coderdojo.org>
2016-02-23 15:48:44 +0000

    Log in so event page can be viewed

--- pages/event.js
+++ pages/event.js
@@ -4,21 +4,23 @@ var eventCommands = {
 		.waitForElementVisible('body');
 		return this;
 	},
+
 	submit: function(){
-		this.waitForElementVisiable('@submitButton', 1000)
-			.click('@submitButton')
-			.api.pause(1000)
+		this.waitForElementVisiable('@submit', 1000)
+			.click('@submit')
+			.api.pause(1000);
 		return this;
 	}
 };
 
 module.exports = {
 	url: function (){
-		return this.api.launchUrl + '/b185ea1a-c2ea-4f8a-8ebf-3f5352a45c08/event-form/22f13c7c-28ca-4be5-9aa7-ea3d9bd12a67';
+		return this.api.launchUrl + '/dashboard/dojo/b185ea1a-c2ea-4f8a-8ebf-3f5352a45c08/event-form/22f13c7c-28ca-4be5-9aa7-ea3d9bd12a67';
 	},
 	commands: [eventCommands],
 	elements:{
-
+		email: { selector: 'input[name="email"]'},
+		password: { selector: 'input[name="password"]'},
 		fromDate: { selector: 'input[name="fromDate"]'},
 		toDate: { selector: 'input[name="toDate"]'}
 	}
--- test/event.js
+++ test/event.js
@@ -2,16 +2,23 @@ module.exports = {
 	'tags': ['event'],
 
 	before : function(browser){
+		var home = browser.page.login();
+	    home.openPage();
+
+	    home.setValue('@email', 'manager@example.com')
+	    .setValue('@password', 'test')
+	    .submit()
+
+	    // open event dojo page
 		browser.page.event().openPage();
 	},
 
 	'Event page ready': function(browser){
 		var eventPage = browser.page.event();
 
-		/*sessionDescription and ticketType missing a class/id*/
-
 		eventPage.expect.element('@fromDate').to.be.present;
 		eventPage.expect.element('@toDate').to.be.present;
+
 	},
 
 	'End Test': function(browser){
